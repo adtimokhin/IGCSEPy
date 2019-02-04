0092e4a74ab518661f53f7755e17290b7699d77b
adtimokhin <adtimokhin@gmail.com>
2019-02-04 11:46:40 +0300

    Assignment Six has been completed

---
+++ AssignmentSix.py
@@ -0,0 +1,47 @@
+# lists
+
+
+master_list, male, female, band, dj = [], [], [], [], []
+
+
+def define_artist(artist):
+    artist_type = input("enter artist\'s type")
+    if len(artist_type) != 1:
+        print("entered type should be one character long")
+        define_artist(artist)
+    if 'M' == artist_type:
+        male.append(artist)
+        return
+    elif 'F' == artist_type:
+        female.append(artist)
+        return
+    elif 'B' == artist_type:
+        band.append(artist)
+        return
+    elif 'D' == artist_type:
+        dj.append(artist)
+        return
+    else:
+        print("entered type of artist was not identified")
+        define_artist(artist)
+
+
+def print_list(name, list):
+    print("\n{0} :".format(name))
+    if len(list) ==0:
+        print("none {0}".format(name.lower()))
+        return
+    for i in list:
+        print(i)
+
+
+for i in range(2):
+    name = input("enter an artist name")
+    master_list.append(name)
+    define_artist(name)
+
+
+print_list("Male vocalists", male)
+print_list("Female vocalists", female)
+print_list("Bands", band)
+print_list("DJs", dj)
